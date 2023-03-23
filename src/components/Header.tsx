07ff56bbe88d5c22235251bdcd6448fa814c9d14
Chris Kalmar <christian.kalmar@gmail.com>
2023-03-24 00:50:06 +0100

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => (
+  <div className="py-6 text-center shadow-2xl">
+    <span className="bg-gradient-to-r from-purple-700 to-blue-500 bg-clip-text text-3xl font-extrabold tracking-tight text-transparent">
+      AI Chat Playground
+    </span>
+  </div>
+);
