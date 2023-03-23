3a587eba5f415b4e8a3a8a042715b7991eb5b07a
Chris Kalmar <christian.kalmar@gmail.com>
2023-03-24 00:50:28 +0100

    implement backend

---
+++ src/server/api/routers/ai.ts
@@ -0,0 +1,68 @@
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+import { Configuration, OpenAIApi } from "openai";
+import { TRPCError } from "@trpc/server";
+import axios from "axios";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+type Message = {
+  role: "user" | "system" | "assistant";
+  content: string;
+};
+
+const messages: Message[] = [];
+
+export const aiRouter = createTRPCRouter({
+  generateText: publicProcedure
+    .input(z.object({ prompt: z.string() }))
+    .mutation(async ({ input }) => {
+      const { prompt } = input;
+
+      messages.push({
+        role: "user",
+        content: prompt,
+      });
+
+      try {
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages,
+        });
+
+        const generatedText = completion.data.choices[0]?.message?.content;
+
+        if (generatedText) {
+          messages.push({
+            role: completion.data.choices[0]?.message?.role ?? "system",
+            content: generatedText,
+          });
+        }
+
+        return {
+          generatedText: generatedText ?? "<no text generated>",
+        };
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+            message: error.response?.data?.error?.message,
+          });
+        }
+
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error instanceof Error ? error.message : "Unknown error",
+        });
+      }
+    }),
+
+  reset: publicProcedure.mutation(() => {
+    messages.length = 0;
+  }),
+});
